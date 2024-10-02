0c40421eebb8c9a9c597c47605a465fdfa19e2ad
Marcosluengo04 <marcos.lgortiz@gmail.com>
2024-10-02 17:18:07 +0200

    a
    a

--- Assets/Scenes/Movimiento.cs
+++ Assets/Scenes/Movimiento.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movimiento : MonoBehaviour
-{   
+    
 
+{
+    [SerializeField] int velocidad = 2;
+    [SerializeField] Vector3 direccion;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,11 @@ public class Movimiento : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //transform.Rotate(direccionRot * velRot * Time.deltaTime);
+        
+        transform.Translate( new Vector3(0 ,0, -1) * velocidad * Time.deltaTime );
+        //transform.rotation()
+
         
     }
 }
